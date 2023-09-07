bdd30341a442d6902e00b2e9d878d617a729a3cc
Dariusz Walda <walda.darek@gmail.com>
2023-09-07 17:06:10 +0200

    Data validations

--- 3_Hard_projects/Flask/flask_market/market/routes.py
+++ 3_Hard_projects/Flask/flask_market/market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 @app.route('/home')
@@ -13,7 +14,17 @@ def marketPage():
     items = Item.query.all()
     return render_template('market.html',items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def registerPage():
     form = RegisterForm()
+    if form.validate_on_submit():
+        userToCreate = User(username=form.username.data,
+                            emailAddress = form.emailAddress.data,
+                            password_hash=form.password1.data)
+        db.session.add(userToCreate)
+        db.session.commit()
+        return redirect(url_for('marketPage'))
+    if form.errors != {}: # if there are not errors from validation
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
     return render_template('register.html',form=form)
