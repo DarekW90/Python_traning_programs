7822d0162804f7e3b230322bbc360b71b4497584
Dariusz Walda <walda.darek@gmail.com>
2023-09-07 16:03:20 +0200

    Flask Forms

--- 3_Hard_projects/Flask/flask_market/market/__init__.py
+++ 3_Hard_projects/Flask/flask_market/market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6a9ecba70ab69e0f08b72a3a'
 db = SQLAlchemy(app)
 
 from market import routes
