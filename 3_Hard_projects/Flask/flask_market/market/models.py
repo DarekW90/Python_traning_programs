52b3045e60768dcfb5b67f3bc15f3ee48673ce0b
Dariusz Walda <walda.darek@gmail.com>
2023-09-09 23:02:51 +0200

    item buy module

--- 3_Hard_projects/Flask/flask_market/market/models.py
+++ 3_Hard_projects/Flask/flask_market/market/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
         if len(str(self.budget)) >= 4:
             return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]} $'
         else:
-            return f'{self.prittier_budget} $'
+            return f"{self.budget} $"
         
     @property
     def password(self):
@@ -32,6 +32,9 @@ class User(db.Model, UserMixin):
         
     def check_password_correction(self,attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+    
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -42,3 +45,8 @@ class Item(db.Model):
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item{self.name}'
+    
+    def buy(self,user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
