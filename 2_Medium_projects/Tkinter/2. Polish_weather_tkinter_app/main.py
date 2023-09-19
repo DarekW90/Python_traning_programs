3e3ba49b342c08ace8710c6cb3c88aeaafea8ced
Dariusz Walda <walda.darek@gmail.com>
2023-09-19 13:36:16 +0200

    app is working fine, now need to set bg img's

--- 2_Medium_projects/Tkinter/2. Polish_weather_tkinter_app/main.py
+++ 2_Medium_projects/Tkinter/2. Polish_weather_tkinter_app/main.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 from tkinter import *
 import requests
-from tkinter import ttk
+import os
+from tkinter import ttk, PhotoImage
+
 
 bg_color = 'lightblue'
 
 root = tk.Tk()
+
+current_file_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(current_file_path)
+label_image = PhotoImage(file=os.path.join(
+    current_directory, 'assets/fog.png'))
+
 
 root.title("Polish Weather")
 root.geometry("400x350")
@@ -18,18 +26,18 @@ def show(*args):
     for row in data:
         if selected_item == row[0]:
             temperature = float(row[2])
-            label.config(text=f"City: {row[0]}\n"
+            label.config(text=f"City: {row[0]}\n\n"
                          f"Observation Time: {row[1]} (hour)\n"
                          f"Temperature: {row[2]} \u2103\n"
                          f"Wind Speed: {row[3]} km/h\n"
                          f"Rain: {row[4]} mm\n"
                          f"Pressure: {row[5]} hPa")
             if temperature > 10:
-                label.configure(bg='green')
-                root.configure(bg='green')
+                label.configure(bg='lightgreen')
+                root.configure(bg='lightgreen')
             else:
-                root.configure(bg='white')
                 label.configure(bg='white')
+                root.configure(bg='white')
 
 
 url = 'https://danepubliczne.imgw.pl/api/data/synop'
@@ -61,12 +69,31 @@ clicked = StringVar()
 # Create button, it will change label text
 option_var = tk.StringVar(value=options[0])
 option_menu = ttk.OptionMenu(
-    root, option_var, *options, command=show)
-option_menu.pack(padx=10, pady=10)
+    root, option_var, *options)
+option_menu.pack(pady=10)
+
+start_button = tk.Button(
+        
+        text="What's the weather ?",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=show,
+        ).pack(pady=10)
 
 # Create Label
-label = Label(root, background=bg_color, text=" ")
+hello_msg = 'Hi!\nMy easy-to-use weather station will allow\nyou to quickly check the current weather\nconditions in Poland.\n\nThe application allows you to \ndownload in real time weather conditions\nfrom the official API of the IMGW website.'
+
+
+label_frame = Frame(root, bg=bg_color, bd=2, relief=SOLID)
+label_frame.pack()
+
+label = Label(label_frame, bg=bg_color, text=hello_msg, font=("Arial", 12))
 label.pack()
+
+
+# label = Label(label_frame, text=" ")
+# label.pack()
 
 # Execute tkinter
 root.mainloop()
