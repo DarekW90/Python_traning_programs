2c778a94b82ede0213c8b4d4ec4e9d3d61be2db0
Dariusz Walda <walda.darek@gmail.com>
2023-09-19 12:34:22 +0200

    add assets and some changes with code

--- 2_Medium_projects/Tkinter/2. Polish_weather_tkinter_app/main.py
+++ 2_Medium_projects/Tkinter/2. Polish_weather_tkinter_app/main.py
@@ -1,19 +1,72 @@
 import tkinter as tk
+from tkinter import *
+import requests
+from tkinter import ttk
 
-bg_color = 'green'
+bg_color = 'lightblue'
 
 root = tk.Tk()
 
 root.title("Polish Weather")
-root.geometry("400x300")
-root.configure(bg='lightblue')
+root.geometry("400x350")
+root.configure(bg=bg_color)
 root.eval("tk::PlaceWindow . center")
-button = tk.Button(root, text="Kliknij mnie")
-button.pack()
 
 
+def show(*args):
+    selected_item = option_var.get()
+    for row in data:
+        if selected_item == row[0]:
+            temperature = float(row[2])
+            label.config(text=f"City: {row[0]}\n"
+                         f"Observation Time: {row[1]} (hour)\n"
+                         f"Temperature: {row[2]} \u2103\n"
+                         f"Wind Speed: {row[3]} km/h\n"
+                         f"Rain: {row[4]} mm\n"
+                         f"Pressure: {row[5]} hPa")
+            if temperature > 10:
+                label.configure(bg='green')
+                root.configure(bg='green')
+            else:
+                root.configure(bg='white')
+                label.configure(bg='white')
+
+
+url = 'https://danepubliczne.imgw.pl/api/data/synop'
+response = requests.get(url)
+
+options = ["Select city closest to you"]
+data = []
+
+for row in response.json():
+    city = row['stacja']
+    observation_time = row['godzina_pomiaru']
+    temperature = row['temperatura']
+    wind_speed = row['predkosc_wiatru']
+    rain = row['suma_opadu']
+    pressure = row['cisnienie']
+
+    options.append(city)
+    data.append([
+        city,
+        observation_time,
+        temperature,
+        wind_speed,
+        rain,
+        pressure
+    ])
+
+clicked = StringVar()
+
+# Create button, it will change label text
+option_var = tk.StringVar(value=options[0])
+option_menu = ttk.OptionMenu(
+    root, option_var, *options, command=show)
+option_menu.pack(padx=10, pady=10)
+
+# Create Label
+label = Label(root, background=bg_color, text=" ")
+label.pack()
+
+# Execute tkinter
 root.mainloop()
-
-
-
-
